504e7eb46389c7d3bc4f57502d7d4e56cf9a7a38
threatHNTR <102164992+threatHNTR@users.noreply.github.com>
2023-10-10 14:50:53 -0400

    Update and rename band_name_generator.py to 1_band_name_generator.py

---
+++ 100_days_python/1_band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator!")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+print("You band name could be " + city + " " + pet + "!")
